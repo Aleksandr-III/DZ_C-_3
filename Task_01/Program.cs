f5ca3bdb700b3996223c591738e4f8e5ce2eb8ae
Александр <popovaleksandr201306@gmail.com>
2022-08-22 02:44:35 +0500

    DZ completed

---
+++ Task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * -1;
+}
+
+string num = Math.Abs(number).ToString();
+
+if (num.Length != 5)
+{
+    Console.WriteLine("Вы ввели не пятизначное число"); 
+}
+
+else if (num[0] == num[4] && num[1] == num[3])
+{
+    Console.WriteLine($"Число {num} является палиндромом");
+}
+
+else
+{
+    Console.WriteLine($"Число {num} не является палиндромом");
+}
---
+++ Task_03/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+// void cube(int x)
+// {
+//     int count = 1;
+//     while ( count <= x)
+//     {
+//         Console.Write($"{(Math.Pow(count, 3))}, ");
+//         count++;
+//     }
+// }
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"{a} -> ");
+// cube(a);
+
+
+// или
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+Console.Write($"{N} -> ");
+for(int i = 1; i <= N; i++)  // (от i ; до i = N ; и какой шаг(в данном случае +1))
+{
+     Console.Write($"{i*i*i}, ");
+}
+
